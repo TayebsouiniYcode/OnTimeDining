427f48f38f60a93f05ae109cc3a55e885e8662ce
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2023-03-14 14:39:08 +0100

    ajouter le model de dash avec les methode toMap, fromMap et toString

--- lib/model/Dash.dart
+++ lib/model/Dash.dart
@@ -2,11 +2,54 @@ import 'Category.dart';
 import 'DashItem.dart';
 
 class Dash {
-  late int id;
-  late String title;
-  late String description;
-  late double price;
-  late String image;
-  late Category category;
-  late List<DashItem> items;
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String image;
+  final int categoryId;
+
+  Dash({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.categoryId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'price': price,
+      'image': image,
+      'categoryId': categoryId,
+    };
+  }
+
+  factory Dash.fromMap(Map<String, dynamic>? map) {
+    if (map == null) {
+      return Dash(
+          id: -1,
+          title: '',
+          description: '',
+          categoryId: -1,
+          price: 00.00,
+          image: '');
+    }
+    return Dash(
+        id: map['id'] ?? '',
+        title: map['title'] ?? '',
+        description: map['description'] ?? '',
+        categoryId: map['categoryId'] ?? '',
+        price: map['price'] ?? '',
+        image: map['image'] ?? '');
+  }
+
+  @override
+  String toString() {
+    return 'Dash { id : $id, title: $title, description: $description, price: $price, categoryId : $categoryId, image name : $image}';
+  }
 }
