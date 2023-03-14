ba2708221cb7ebff68e6af1cc73cd5315e4c2d1e
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2023-03-14 14:29:53 +0100

    ajouter model category avec les methodes toString, toMap et FromMap

--- lib/model/Category.dart
+++ lib/model/Category.dart
@@ -1,6 +1,39 @@
 class Category {
-  late int id;
-  late String title;
-  late String description;
-  late String image;
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+
+  Category({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': title,
+      'description': description,
+      'image': image,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic>? map) {
+    if (map == null) {
+      return Category(id: -1, title: '', description: '', image: '');
+    }
+
+    return Category(
+        id: map['id'] ?? '',
+        title: map['title'] ?? '',
+        description: map['description'] ?? '',
+        image: map['image'] ?? '');
+  }
+
+  @override
+  String toString() {
+    return 'Category { id: $id, title: $title, description: $description, image name : $image}';
+  }
 }
